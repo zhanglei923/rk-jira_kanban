de992af5be2cb03a0ca92b03d157637fa6cae9dc
zhanglei <zhanglei@xiaoshouyi.com>
2020-01-19 17:15:57 +0800

    up

--- kanban-website/index.js
+++ kanban-website/index.js
@@ -1,6 +1,8 @@
 $(()=>{
     $('#parseBtn').on('click', ()=>{
         let content = $('#content').val();
+        content = _.trim(content);
+        if(!content) return;
         let idList = content.match(/[A-Z]{1,}\-[0-9]{1,}/g)
         console.log(idList)
 
@@ -11,10 +13,37 @@ $(()=>{
                 id_list: idList.join(',')
             },
             success: function( response ) {
-              console.log(response)
+              showIssues(response)
             },
             error:function(ajaxObj,msg,err){
             }
         });
     })
 })
+let showIssues = (records)=>{
+    let html = `<table>`;
+    for(let id in records){
+        let record = records[id];
+        let summary = record.summary;
+        // status: issue.fields.status.name,
+        // summary: issue.fields.summary,
+        // assignee: issue.fields.assignee.name,
+        // reporter: issue.fields.reporter.name,
+        // created: issue.fields.created,
+        // updated: issue.fields.updated,
+        console.log(record)
+
+        let li = `<tr id="${id}">
+                    <td>${summary.status}</td>
+                    <td>${summary.summary}</td>
+                    <td>${summary.assignee}</td>
+                    <td>${summary.reporter}</td>
+                    <td>${moment(summary.created).format('YYYY-MM-DD hh:mm')}</td>
+                    <td>${moment(summary.updated).format('YYYY-MM-DD hh:mm')}</td>
+                </tr>`
+        html += li;
+    }
+    html += `</table>`
+
+    $('#jira_list').html(html);
+}
