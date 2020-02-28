e5313b868b757dc3af7e8a74d5cbfbf60753ac48
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-28 17:11:32 +0800

    up

--- kanban-server/server.js
+++ kanban-server/server.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
     res.set('Cache-Control', 'no-cache')
     next();
 });
-app.use('/website', express.static(webPath));//注意：必须在全局拦截器之后，否则拦截器无法运行
+app.use('/', express.static(webPath));//注意：必须在全局拦截器之后，否则拦截器无法运行
 
 app.get('/action/jira-info',function(req, res){
     res.send(jiraUtil.getJiraInfo())
