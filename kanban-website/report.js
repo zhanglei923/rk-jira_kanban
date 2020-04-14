7f4f6e739cb5d74f93ee57f4f9d8944f5a7d79fe
zhanglei <zhanglei@xiaoshouyi.com>
2020-04-15 00:38:24 +0800

    up

--- kanban-website/report.js
+++ kanban-website/report.js
@@ -1,8 +1,9 @@
 let generateSprintStoryReport = (records)=>{
     let totalpoints=0;
     let rpt_assignees = {};
-    let rpt_types={}
-    let rpt_status={}
+    let rpt_types={};
+    let rpt_status={};
+    let rpt_reporter={};
     for(let i=0;i<records.length;i++){
         let record = records[i];
         let id = record.id;
@@ -10,9 +11,11 @@ let generateSprintStoryReport = (records)=>{
         let summary = record.summary;
 
         let status = summary.status;
+        let reporter = summary.reporter;
         let statusname = status.toLowerCase().replace(/\s/g, '');
         let assignee = summary.assignee?summary.assignee:'';
         let issueTypeName = summary.issueTypeName?summary.issueTypeName:'';
+        //
         if(!rpt_assignees[assignee]) rpt_assignees[assignee] = {
             totalpoints: 0
         }
@@ -26,13 +29,18 @@ let generateSprintStoryReport = (records)=>{
         if(!rpt_status[status]) rpt_status[status] = {
             totalpoints: 0
         }
-        rpt_status[status].totalpoints += summary.storypoint;
+        //
+        if(!rpt_reporter[reporter]) rpt_reporter[reporter] = {
+            totalpoints: 0
+        }
+        rpt_reporter[reporter].totalpoints += summary.storypoint;
 
         totalpoints += summary.storypoint;
     }
     showSprintStoryReport(totalpoints, rpt_assignees)
     showSprintStoryReport(totalpoints, rpt_types)
     showSprintStoryReport(totalpoints, rpt_status)
+    showSprintStoryReport(totalpoints, rpt_reporter)
 }
 let showSprintStoryReport = (totalpoints, rpt_assignees)=>{
     let html = `<div><div>Story Points:</div>`;
