ada74330de9acd8721d7b305b776edf10e8ddf35
zhanglei <zhanglei@xiaoshouyi.com>
2020-04-28 01:01:00 +0800

    up

--- kanban-website/report3.js
+++ kanban-website/report3.js
@@ -3,7 +3,12 @@ let reportWholeTeamInfo = (records)=>{
     let report_point = {};
     let report_num = {};
     let totalpoints = 0;
+    let totalpoints_done = 0;
+    let totalpoints_notdone = 0;
     let totalnum = 0;
+    let totalnum_done = 0;
+    let totalnum_notdone = 0;
+
     for(let i=0;i<records.length;i++){
         let record = records[i];
         if(record.notExist) continue;
@@ -22,26 +27,31 @@ let reportWholeTeamInfo = (records)=>{
         };
         if(summary.devIsDone){
             report_point[summary.stretchorcommited].done += summary.storypoint;
+            totalpoints_done += summary.storypoint;
             report_num[summary.stretchorcommited].done++;
+            totalnum_done++;
         }else{
             report_point[summary.stretchorcommited].notdone += summary.storypoint;
+            totalpoints_notdone += summary.storypoint;
             report_num[summary.stretchorcommited].notdone++;
+            totalnum_notdone++;
         }
         report_point[summary.stretchorcommited].total += summary.storypoint;
         report_num[summary.stretchorcommited].total ++;
         totalpoints += summary.storypoint;
         totalnum++;
     }
-    let rpttxt = `
-        全部点数：${totalpoints}点<br>
-        全部commited点数：${report_point.commited.total}点，完成${report_point.commited.done}，未完成${report_point.commited.notdone}，完成率${_percentage(report_point.commited.done, report_point.commited.total)}%<br>
-        全部stretched点数：${report_point.stretched.total}点，完成${report_point.stretched.done}，未完成${report_point.stretched.notdone}，完成率${_percentage(report_point.stretched.done, report_point.stretched.total)}%<br>
+    let rpttxt = ``;
+    rpttxt += `
+        全部点数：${totalpoints} = <span class="number_done">${totalpoints_done}</span> + <span class="number_open">${totalpoints_notdone}</span>，完成率${_percentage(totalpoints_done, totalpoints)}%<br>
+        &emsp;commited点数：${report_point.commited.total} = <span class="number_done">${report_point.commited.done}</span> + <span class="number_open">${report_point.commited.notdone}</span>，完成率${_percentage(report_point.commited.done, report_point.commited.total)}%<br>
+        &emsp;stretched点数：${report_point.stretched.total} = <span class="number_done">${report_point.stretched.done}</span> + <span class="number_open">${report_point.stretched.notdone}</span>，完成率${_percentage(report_point.stretched.done, report_point.stretched.total)}%<br>
     `
     rpttxt += `<hr>`
     rpttxt += `
-        全部个数：${totalnum}个<br>
-        全部commited个数：${report_num.commited.total}个，完成${report_num.commited.done}，未完成${report_num.commited.notdone}，完成率${_percentage(report_num.commited.done, report_num.commited.total)}%<br>
-        全部stretched个数：${report_num.stretched.total}个，完成${report_num.stretched.done}，未完成${report_num.stretched.notdone}，完成率${_percentage(report_num.stretched.done, report_num.stretched.total)}%<br>
+        全部个数：${totalnum} = <span class="number_done">${totalnum_done}</span> + <span class="number_open">${totalnum_notdone}</span>，完成率${_percentage(totalnum_done, totalnum)}%<br>
+        &emsp;commited个数：${report_num.commited.total} = <span class="number_done">${report_num.commited.done}</span> + <span class="number_open">${report_num.commited.notdone}</span>，完成率${_percentage(report_num.commited.done, report_num.commited.total)}%<br>
+        &emsp;stretched个数：${report_num.stretched.total} = <span class="number_done">${report_num.stretched.done}</span> + <span class="number_open">${report_num.stretched.notdone}</span>，完成率${_percentage(report_num.stretched.done, report_num.stretched.total)}%<br>
     `
     console.log(report_point)
     $('#teamperformance_list').html(rpttxt)
